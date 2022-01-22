82cc7bbb6751bb033a2bea95fc2fde0f2d4b2349
Danika H Jaskulak-Gonzalez <danikaherminise@gmail.com>
2022-01-21 17:45:50 -0800

    edited code

--- rearrange.py
+++ rearrange.py
@@ -1,10 +1,14 @@
-import random
+import random, sys
+
+input_str = sys.argv[1:]
 
 def rearrange(str):
   rand_list = str.split(' ')
   random.shuffle(rand_list)
-  return rand_list
+  output = ''
+  for i in len(rand_list):
+    output += str(i) + ' '
+  return output
 
 if __name__ == '__main__':
-    rearranged = rearrange(input())
-    print(' '.join(rearranged))
+  print(rearrange(str(input_str)))
