c3d210a8249cb58cb68fc227fb0b48846ad546c7
Danika H Jaskulak-Gonzalez <danikaherminise@gmail.com>
2022-02-28 17:06:23 -0800

    dictogram

--- Code/dictogram.py
+++ Code/dictogram.py
@@ -21,15 +21,33 @@ class Dictogram(dict):
     def add_count(self, word, count=1):
         """Increase frequency count of given word by given count amount."""
         # TODO: Increase word frequency by count
+        if word in self:
+            self[word] += count
+            self.tokens += count
+        else:
+            self[word] = count
+            self.types += 1
+            self.tokens += count
 
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
         # TODO: Retrieve word frequency count
+        return self.get(word, 0)
 
     def sample(self):
         """Return a word from this histogram, randomly sampled by weighting
         each word's probability of being chosen by its observed frequency."""
         # TODO: Randomly choose a word based on its frequency in this histogram
+        sample_number = random.randrange(0, sum(self.values()))
+        index_pos = 0 
+        sample_word = None
+
+        for word in self:
+            if index_pos <= sample_number:
+                index_pos += self[word]
+                sample_word = word 
+        
+        return sample_word
 
 
 def print_histogram(word_list):
